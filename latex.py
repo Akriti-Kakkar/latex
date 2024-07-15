18b494b798656d9f032785eefb502b43b766334d
Akriti-Kakkar <89967832+Akriti-Kakkar@users.noreply.github.com>
2024-07-15 23:17:32 +0530

    Add files via upload

---
+++ latex.py
@@ -0,0 +1,24 @@
+from termcolor import colored, cprint
+
+import streamlit as st
+
+import sys
+
+text = colored("Latex Format", "green")
+text1 = colored("new stuff", "red")
+
+st.latex(text)
+
+st.write(text)
+
+st.html(text)
+
+st.markdown(text)
+
+st.write(f"${{text}}$", f"${{text1}}$")
+
+st.button(f''':red[${{text}}$]
+          
+          :blue[${{text1}}$]''')
+
+st.write(r"${}$".format(text))
